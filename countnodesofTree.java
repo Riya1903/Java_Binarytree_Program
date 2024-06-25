6d98065b59d70bea2708c15cb6a1a62ed86fdc95
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-06-25 22:16:45 +0530

    Add files via upload

---
+++ countnodesofTree.java
@@ -0,0 +1,50 @@
+public class countodesofTree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data)
+        {
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static int height(Node root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int lh=height(root.left);
+        int rh=height(root.right);
+        return Math.max(lh,rh+1);
+    }
+    public static int countnodes(Node root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int lc=countnodes(root.left);
+        int rc=countnodes(root.right);
+        return lc+rc+1;
+    }
+    public static void main(String args[])
+{
+/*    1
+     / \
+    2   3
+   / \ / \
+  4   56  7
+*/
+Node root=new Node(1);
+root.left=new Node(2);
+root.right=new Node(3);
+root.left.left=new Node(4);
+root.left.right=new Node(5);
+root.right.left=new Node(6);
+root.right.right=new Node(7);
+System.out.println(countnodes(root));
+}
+}
